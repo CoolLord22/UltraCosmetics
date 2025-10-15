54b929c906dc72e7405a2813e112cbd7e7a62202
CoolLord22 <mastermodmaker@yahoo.com>
2025-10-15 04:12:45 -0400

    Create command cosmetic
    Handles running any of our custom command triggered cosmetics

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/CommandCosmetic.java
@@ -0,0 +1,33 @@
+package be.isach.ultracosmetics.cosmetics;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.cosmetics.type.CosmeticType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+import org.bukkit.Bukkit;
+
+public abstract class CommandCosmetic<T extends CosmeticType<?>> extends Cosmetic<T> {
+    protected String command;
+
+    public CommandCosmetic(UltraPlayer owner, T type, UltraCosmetics ultraCosmetics) {
+        super(owner, type, ultraCosmetics);
+    }
+
+    protected abstract String getResetCommand();
+
+    @Override
+    public void onEquip() {
+        runCommand(command);
+    }
+
+    @Override
+    public void onClear() {
+        runCommand(getResetCommand());
+    }
+
+    protected void runCommand(String command) {
+        if(command.contains("%player"))
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%", getPlayer().getName()));
+        else
+            Bukkit.dispatchCommand(getPlayer(), command);
+    }
+}
