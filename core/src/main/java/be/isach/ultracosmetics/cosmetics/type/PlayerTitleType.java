271689f29720e66bee57b14212e95d3b39897509
CoolLord22 <mastermodmaker@yahoo.com>
2025-10-15 17:58:23 -0400

    Fix list to enable and default desc

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PlayerTitleType.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PlayerTitleType.java
@@ -10,12 +10,12 @@ import java.util.Arrays;
 
 public class PlayerTitleType extends CosmeticType<PlayerTitle> {
     private static final String keys =
-            "bs_milestone_100blocksfound,bs_milestone_level10,bs_milestone_10wins" + // Common
-            "bs_milestone_500blocksfound,bs_milestone_level25,bs_milestone_50wins" + // Uncommon
-            "bs_milestone_1000blocksfound,bs_milestone_level50,bs_milestone_150wins" + // Rare
-            "bs_milestone_2500blocksfound,bs_milestone_level100,bs_milestone_300wins" + // Epic
-            "bs_milestone_5000blocksfound,bs_milestone_level250,bs_milestone_600wins" + // Legendary
-            "bs_milestone_10000blocksfound,bs_milestone_level500,bs_milestone_1000wins" + // Mythic
+            "bs_milestone_100blocksfound,bs_milestone_level10,bs_milestone_10wins," + // Common
+            "bs_milestone_500blocksfound,bs_milestone_level25,bs_milestone_50wins," + // Uncommon
+            "bs_milestone_1000blocksfound,bs_milestone_level50,bs_milestone_150wins," + // Rare
+            "bs_milestone_2500blocksfound,bs_milestone_level100,bs_milestone_300wins," + // Epic
+            "bs_milestone_5000blocksfound,bs_milestone_level250,bs_milestone_600wins," + // Legendary
+            "bs_milestone_10000blocksfound,bs_milestone_level500,bs_milestone_1000wins," + // Mythic
             "betatester"; // Extra
     private static final String[] titlename = keys.split(",");
     private final String command;
@@ -24,7 +24,8 @@ public class PlayerTitleType extends CosmeticType<PlayerTitle> {
         super(Category.PLAYER_TITLE, configName, XMaterial.NAME_TAG, PlayerTitle.class);
         this.command = "playertitle " + configName;
         if (GENERATE_MISSING_MESSAGES) {
-            MessageManager.addMessage(getConfigPath() + ".name", "[" + configName + "]");
+            MessageManager.addMessage(getConfigPath() + ".name", "<gray>[]");
+            MessageManager.addMessage(getConfigPath() + ".description", "\"<white>500 Blocks Found \\n<dark_gray>ᴜɴᴄᴏᴍᴍᴏɴ\"");
         }
     }
 
